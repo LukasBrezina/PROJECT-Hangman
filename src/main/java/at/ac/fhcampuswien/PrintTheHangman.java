00bb5b2e4af4a8f44664a94d9926b6caacf42c39
LukasBrezina <lukas.brezina@stud.fh-campuswien.ac.at>
2023-01-23 13:45:27 +0100

    Last Changes

--- src/main/java/at/ac/fhcampuswien/PrintTheHangman.java
+++ src/main/java/at/ac/fhcampuswien/PrintTheHangman.java
@@ -1,154 +1,99 @@
 package at.ac.fhcampuswien;
 
-public class PrintTheHangman
-{
-    public static void printHangman(int wrongCount)
-    {
+public class PrintTheHangman {
+    public static void printHangman(int wrongCount) {
 
-        switch (wrongCount)
-        {
+        switch (wrongCount) {
             case 1:
-                System.out.println("    ____");
-                System.out.println("   |    |");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("___|___");
-                break;
+                System.out.println("/"); break;
             case 2:
-                System.out.println("    ____");
-                System.out.println("   |    |");
-                System.out.println("   |    O");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("___|___");
-                break;
+                System.out.println("/\\"); break;
             case 3:
-                System.out.println("    ____");
-                System.out.println("   |    |");
-                System.out.println("   |    O");
-                System.out.println("   |    |");
-                System.out.println("   |    |");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("___|___");
-                break;
+                System.out.println(" | ");
+                System.out.println(" | ");
+                System.out.println(" | ");
+                System.out.println(" | ");
+                System.out.println(" | ");
+                System.out.println("/  \\"); break;
             case 4:
-                System.out.println("    ____");
-                System.out.println("   |    |");
-                System.out.println("   |    O");
-                System.out.println("   |   /|");
-                System.out.println("   |    |");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("___|___");
-                break;
+                System.out.println(" ----------");
+                System.out.println(" | ");
+                System.out.println(" | ");
+                System.out.println(" | ");
+                System.out.println(" | ");
+                System.out.println(" | ");
+                System.out.println("/  \\"); break;
             case 5:
-                System.out.println("    ____");
-                System.out.println("   |    |");
-                System.out.println("   |    O");
-                System.out.println("   |   /|\\");
-                System.out.println("   |    |");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("___|___");
-                break;
+                System.out.println(" ----------");
+                System.out.println(" | /");
+                System.out.println(" |/ ");
+                System.out.println(" | ");
+                System.out.println(" | ");
+                System.out.println(" | ");
+                System.out.println("/  \\"); break;
             case 6:
-                System.out.println("    ____");
-                System.out.println("   |    |");
-                System.out.println("   |    O");
-                System.out.println("   |   /|\\");
-                System.out.println("   |    |");
-                System.out.println("   |   /");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("___|___");
-                break;
+                System.out.println(" ----------");
+                System.out.println(" | /      |");
+                System.out.println(" |/ ");
+                System.out.println(" | ");
+                System.out.println(" | ");
+                System.out.println(" | ");
+                System.out.println("/  \\"); break;
             case 7:
-                System.out.println("    ____");
-                System.out.println("   |    |");
-                System.out.println("   |    O");
-                System.out.println("   |   /|\\");
-                System.out.println("   |    |");
-                System.out.println("   |   / \\");
-                System.out.println("   |");
-                System.out.println("   |");
-                System.out.println("___|___");
-                break;
-            case 8:
-                System.out.println("    ____");
-                System.out.println("   |    |");
-                System.out.println("   |    O");
-                System.out.println("   |   /|\\");
-                System.out.println("   |    |");
-                System.out.println("   |   / \\");
-                System.out.println("   |   /");
-                System.out.println("   |");
-                System.out.println("___|___");
-                break;
-            case 9:
-                System.out.println("    ____");
-                System.out.println("   |    |");
-                System.out.println("   |    O");
-                System.out.println("   |   /|\\");
-                System.out.println("   |    |");
-                System.out.println("   |   / \\");
-                System.out.println("   |   / \\");
-                System.out.println("   |");
-                System.out.println("___|___");
-                break;
-            case 10:
-                System.out.println("    ____");
-                System.out.println("   |    |");
-                System.out.println("   |    O");
-                System.out.println("   |   /|\\");
-                System.out.println("   |    |");
-                System.out.println("   |   / \\");
-                System.out.println("   |  ");
-                System.out.println("   |");
-                System.out.println("___|___");
-                break;
-            case 11:
-                System.out.println("    ____");
-                System.out.println("   |    |");
-                System.out.println("   |    O");
-                System.out.println("   |   /|\\");
-                System.out.println("   |    |");
-                System.out.println("   |   / \\");
-                System.out.println("   |  /   \\");
-                System.out.println("   |");
-                System.out.println("___|___");
-                break;
-            case 12:
-                System.out.println("    ____");
-                System.out.println("   |    |");
-                System.out.println("   |    O");
-                System.out.println("   |   /|\\");
-                System.out.println("   |    |");
-                System.out.println("   |   / \\");
-                System.out.println("   |  /   \\");
-                System.out.println("   |");
-                System.out.println("___|___");
-                System.out.println("You lose!");
-                break;
+                System.out.println(" ----------");
+                System.out.println(" | /      |");
+                System.out.println(" |/       O");
+                System.out.println(" | ");
+                System.out.println(" | ");
+                System.out.println(" | ");
+                System.out.println("/  \\"); break;
+            case 8 :
+                System.out.println(" ----------");
+                System.out.println(" | /      |");
+                System.out.println(" |/       O");
+                System.out.println(" |        |");
+                System.out.println(" |        |");
+                System.out.println(" | ");
+                System.out.println("/  \\"); break;
+            case 9 :
+                System.out.println(" ----------");
+                System.out.println(" | /      |");
+                System.out.println(" |/       O");
+                System.out.println(" |       \\|");
+                System.out.println(" |        |");
+                System.out.println(" | ");
+                System.out.println("/  \\"); break;
+            case 10 :
+                System.out.println(" ----------");
+                System.out.println(" | /      |");
+                System.out.println(" |/       O");
+                System.out.println(" |       \\|/");
+                System.out.println(" |        |");
+                System.out.println(" | ");
+                System.out.println("/  \\"); break;
+            case 11 :
+                System.out.println(" ----------");
+                System.out.println(" | /      |");
+                System.out.println(" |/       O");
+                System.out.println(" |       \\|/");
+                System.out.println(" |        |");
+                System.out.println(" |       /");
+                System.out.println("/  \\"); break;
+            case 12 :
+                System.out.println(" ----------");
+                System.out.println(" | /      |");
+                System.out.println(" |/       O");
+                System.out.println(" |       \\|/");
+                System.out.println(" |        |");
+                System.out.println(" |       / \\");
+                System.out.println("/  \\"); break;
+
         }
     }
 }
 
 
-
-
-
-//     _________
+//     ___
 //     | /     |
 //     |/      O
 //     |      \|/
